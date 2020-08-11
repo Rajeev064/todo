6196ebad9ba56370e54112f4ab24647311d21469
Rajeev064 <rs3167196@gmail.com>
2020-08-11 18:21:31 +0530

    Register validation added

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 import os
+import random
 from datetime import datetime
+from flask_bootstrap import Bootstrap 
+from flask_mail import Mail, Message
+import smtplib
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zyfoxoolnlkfez:a41afb777694a73241c20df5d23547da59d92fdf410ab42476656bbc06e6200e@ec2-34-233-226-84.compute-1.amazonaws.com:5432/d1scfbvnegodr2'
+app.config.update(
+    DEBUG=False,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'rajeevdemoprojects@gmail.com',
+	MAIL_PASSWORD = 'Rajeev_102'
+
+
+)
+app.secret_key = "user_new_program"
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 class login(db.Model):
     id = db.Column(db.Integer,primary_key = True)
@@ -36,14 +54,29 @@ def register():
         new_username = request.form['username']
         email = request.form['email']
         new_password = request.form['password']
-        new_contact = login(username = new_username,email_id = email,password = new_password)
+        #new_contact = login(username = new_username,email_id = email,password = new_password)
         r = login.query.filter_by(email_id = email).first()
         if r == None:
             try:
-                db.session.add(new_contact)
-                db.session.commit()
-                flash("Email Added ,now login")
-                return render_template('login.html')
+                otp = random.randint(100000,999999)
+                # otp = 123456
+                try:
+                    msg = Message("Send Mail Tutorial!",sender="rajeevdemoprojects@gmail.com",recipients= [email])
+                    msg.body = "Hello {}" .format(new_username)
+                    msg.html = '<h1 style="color:black">Your Verification Code is {}</h1><h3>If you are not logging in To Do manager ,please ignore the message.Do not Share Your code and Password with anyone ,it may result in loss of your personal details</h3>'.format(otp)     
+                    mail.send(msg)
+                except Exception as e:
+                    print(e)
+                #db.session.add(new_contact)
+                #db.session.commit()
+                # check =  login.query.filter_by(email_id = email).first()
+                # session['USERNAME'] = check.id
+                print(otp)
+                session['OTP'] = otp
+                session['E'] = email
+                session['SECRETMSG'] = new_password
+                session['USER'] = new_username
+                return render_template('verify.html')
             except:
                 return 'error in registering '
 
@@ -55,6 +88,32 @@ def register():
     else:
         return render_template('register.html')
 
+
+@app.route('/verify',methods=['POST','GET'])
+def verify():
+    if request.method == 'POST' :
+        if "OTP" in session:
+            send_code = session['OTP']
+            code = request.form['password']
+            if send_code == send_code:
+                new_username = session['USER']
+                email = session['E']
+                new_password = session['SECRETMSG']
+                new_contact = login(username = new_username,email_id = email,password = new_password)
+                db.session.add(new_contact)
+                db.session.commit()
+                check =  login.query.filter_by(email_id = email).first()
+                session['USERNAME'] = check.id
+                return render_template('index.html')
+            else :
+                return 'CODE incorrect'
+        else:
+            return render_template('register.html')
+    else:
+        if "OTP" in session:
+            return render_template('verify.html')
+        else:
+            return render_template('register.html')
 
 @app.route('/login',methods=['POST','GET'])
 def user_login():
@@ -154,6 +213,4 @@ def update(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-app.secret_key = "user_new_program"
 
